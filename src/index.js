ac1fd06152a99d38b9b480b5f846018bdb4128ed
Charlie Henaff <charlie.henaff@gmail.com>
2022-01-25 23:57:10 +0100

    Remove base_url

--- src/index.js
+++ src/index.js
@@ -27,9 +27,8 @@ export const APP_CONST = {
     },
 };
 
-export const basePath = new URL(process.env.REACT_APP_BASE_URL).pathname;
-console.log(basePath); // TODO: remove
-export const history = createBrowserHistory({basename: basePath});
+console.log(window.location.pathname); // TODO: remove
+export const history = createBrowserHistory({basename: window.location.pathname});
 export const store = createStore(
     combineReducers({
       app: appState,
